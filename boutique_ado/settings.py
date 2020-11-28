7fbe3a4efbd00297185b0e6e7f38749ae5721216
michaelmagee <59846293+michaelmagee@users.noreply.github.com>
2020-11-28 09:18:35 -0500

    More alll-auth additions per video and debug config

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -80,6 +80,17 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1         # for all-auth
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # Username OR email address
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/sucess'
+
+
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 
