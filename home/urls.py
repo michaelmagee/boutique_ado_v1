ca0e918054b7faed152b69db853f24e1d8482842
michaelmagee <59846293+michaelmagee@users.noreply.github.com>
2020-11-29 14:59:02 -0500

    added home app and some templates

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+""" This is our Home urls """
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="home")
+]
---
+++ home/views.py
@@ -0,0 +1,8 @@
+""" Describe this """
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    """ Index page view """
+    return render(request, "home/index.html")
