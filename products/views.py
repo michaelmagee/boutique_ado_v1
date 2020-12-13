fa8785a084f373167870aefe326974de03a0c2d7
michaelmagee <59846293+michaelmagee@users.noreply.github.com>
2020-12-13 15:11:09 -0500

    added sorting and product counts

--- products/views.py
+++ products/views.py
@@ -25,6 +25,9 @@ def all_products(request):
                 products = products.annotate(lower_name='name'.lower())
                 # From CI products = products.annotate(lower_name=Lower('name'))
 
+            if sortkey == "category":
+                sortkey = "category__name"
+
             if "direction" in request.GET:
                 direction = request.GET["direction"]
                 if direction == "desc":
