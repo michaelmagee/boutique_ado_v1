04d308ed9c1e1e5ca4d2cbf9b13e3ed6a29f4555
michaelmagee <mikemagee@comcast.net>
2021-01-08 19:35:10 +0000

    Product delete completed

--- products/views.py
+++ products/views.py
@@ -82,9 +82,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -119,3 +119,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delere a product fromthe store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+
+    messages.success(request, 'Successfully deleted product!')
+
+
+    return  redirect(reverse("products"))
