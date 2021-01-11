fe4414b00ce3482550d5cfd4f4c933d8ebd901b3
michaelmagee <mikemagee@comcast.net>
2021-01-11 18:58:03 +0000

    trace aws env

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -196,7 +196,8 @@ Static files will be collected into a static folder in our s3 bucket.
 """
 
 if 'USE_AWS' in os.environ:
-        # Cache control
+    print ("Aws Detected")
+    # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
