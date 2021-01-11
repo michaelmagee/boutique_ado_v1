b466485887af38580b4f4f77800fd1c793a5ece8
michaelmagee <mikemagee@comcast.net>
2021-01-11 17:31:26 +0000

    Added S3 Cache Control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -196,6 +196,12 @@ Static files will be collected into a static folder in our s3 bucket.
 """
 
 if 'USE_AWS' in os.environ:
+        # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'magee-boutique-ado-v1'
     AWS_S3_REGION_NAME = 'us-east-2'
